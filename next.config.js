81e3ea07ad656f0f7269356a5f4a3fdfceaafc3f
Kijedi <wkijedi@gmail.com>
2023-09-15 16:58:51 +0300

    Added logo

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     
-    domains: ['appwrite.tikobaze.co.ke'],
+    domains: ['appwrite.tikobaze.co.ke', 'studio.tailwindui.com'],
   },
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -5,32 +5,38 @@ import localFont from "next/font/local";
 const mona = localFont({
   src: [
     {
-      path: "../src/fonts/MonaSans-Bold.woff2",
+      path: "../src/fonts/MonaSansExpanded-ExtraBold.woff2",
       weight: "700",
       style: "normal",
       variable: "--font-sans",
     },
     {
-      path: "../src/fonts/MonaSans-SemiBold.woff2",
+      path: "../src/fonts/MonaSansExpanded-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+      variable: "--font-sans",
+    },
+    {
+      path: "../src/fonts/MonaSansExpanded-Medium.woff2",
       weight: "500",
       style: "normal",
       variable: "--font-sans",
     },
     {
-      path: "../src/fonts/MonaSans-Medium.woff2",
+      path: "../src/fonts/MonaSansExpanded-Regular.woff2",
       weight: "400",
       style: "normal",
       variable: "--font-sans",
     },
     {
-      path: "../src/fonts/MonaSans-Regular.woff2",
+      path: "../src/fonts/MonaSansExpanded-Light.woff2",
       weight: "300",
       style: "normal",
       variable: "--font-sans",
     },
     {
-      path: "../src/fonts/MonaSans-Light.woff2",
-      weight: "100",
+      path: "../src/fonts/MonaSansExpanded-ExtraLight.woff2",
+      weight: "200",
       style: "normal",
       variable: "--font-sans",
     },
