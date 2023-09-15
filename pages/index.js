a8fbb88d5c0d84167bd59e0a63923da551403ad3
Kijedi <wkijedi@gmail.com>
2023-09-15 17:30:46 +0300

    Ger

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import Team from "../components/UI/Team";
 import About from "../components/About";
-import Services from "../components/Services";
 
 export default function Home() {
   const router = useRouter();
@@ -10,7 +9,6 @@ export default function Home() {
       <img src="/images/banner.jpg" className="w-full object-cover" />
       <div className="">
         <About/>
-        <Services/>
         <Team />
         <p className="text-center pt-10">
           Made with ❤️&nbsp; by&nbsp;
