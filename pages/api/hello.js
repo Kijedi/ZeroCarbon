a8d9e99749be1b0972cf8dbf2f7748043f73456e
Kijedi <wkijedi@gmail.com>
2023-09-19 13:48:17 +0300

    Okay

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import nodemailer from "nodemailer";
 
-export default function handler (req, res) { 
-  res.status(200).json({ "events": [] });
+export default function handler(req, res) {
+  const transporter = nodemailer.createTransport({
+    host: "mail.violettechies.co.ke",
+    port: 465,
+    secure: true, // upgrade later with STARTTLS
+    auth: {
+      user: "hello@violettechies.co.ke",
+      pass: "Wesleigh#1?",
+    },
+  });
+
+  // Set up email data
+  let options = {
+    from: "hello@violettechies.co.ke", // replace with your email address
+    to: "wkijedi@gmail.com", // replace with the recipient's email address
+    subject: "Test Email from Node.js",
+    text: "Hello world!", // plain text body
+    html: "<p>Hello world!</p>", // HTML body
+  };
+
+  const sendMail = async () => {
+    await transporter.sendMail(options);
+  };
+  sendMail();
+
+  res.status(200).json({ events: [] });
 }
